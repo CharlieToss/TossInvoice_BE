9de35d65bc55cab1d874e42eecac7892350143f1
jk99uu <jang.99.uu@gmail.com>
2026-05-17 12:11:14 +0900

    fix: @EnableJpaAuditing 어노테이션 추가 (#3)

--- src/main/java/hankyung/tossinvoice/TossinvoiceApplication.java
+++ src/main/java/hankyung/tossinvoice/TossinvoiceApplication.java
@@ -2,9 +2,9 @@ package hankyung.tossinvoice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class TossinvoiceApplication {
 
