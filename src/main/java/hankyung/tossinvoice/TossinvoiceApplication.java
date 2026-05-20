a7352348325d440dd7e65973a75052c2cbccd8c5
jk99uu <jang.99.uu@gmail.com>
2026-05-20 11:31:33 +0900

    feat: createdAt KST로 변경

--- src/main/java/hankyung/tossinvoice/TossinvoiceApplication.java
+++ src/main/java/hankyung/tossinvoice/TossinvoiceApplication.java
@@ -2,11 +2,9 @@ package hankyung.tossinvoice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-@EnableJpaAuditing
 @SpringBootApplication
-public class 	TossinvoiceApplication {
+public class TossinvoiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TossinvoiceApplication.class, args);
